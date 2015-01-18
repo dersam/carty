6e31c79e191714b1632146af36f51fcc6cb06713
Sam Schmidt <samuel@dersam.net>
2015-01-18 12:41:32 -0500

    fixed loader url path

--- src/views/shop.blade.php
+++ src/views/shop.blade.php
@@ -33,7 +33,7 @@
                 <button data-role="add-product" data-incart="yes" data-product="{{$product->id}}" class="btn btn-default" style="width:100%">In Cart</button>
                 @endif
                 <div data-role="loader" data-product="{{$product->id}}" style="display: none">
-                    {{HTML::image('packages/Dersam/Carty/img/button-loader.gif','',array('class'=>'img-responsive'))}}
+                    {{HTML::image('packages/dersam/carty/img/button-loader.gif','',array('class'=>'img-responsive'))}}
                 </div>
             </div>
         </div>
@@ -41,7 +41,7 @@
     </div>
 </div>
 <div id="loader" style="display: none">
-    {{HTML::image('packages/Dersam/Carty/img/button-loader.gif','',array('class'=>'img-responsive'))}}
+    {{HTML::image('packages/dersam/carty/img/button-loader.gif','',array('class'=>'img-responsive'))}}
 </div>
 @stop
 
